433f1cf253e67f5c280e0d376ee59de13db26965
Naty Francisco <fnaty629@gmail.com>
2022-08-16 23:13:13 -0400

    Login Simple JS
    Creación de un login sencillo

---
+++ Login Simple/JS/app.js
@@ -0,0 +1,27 @@
+
+//Funcion de nuestro login
+function login(){
+
+
+    //Declaracion de variables para llenar los campos
+    var user = document.getElementById("usuario").value
+    var clave = document.getElementById("clave").value
+
+
+    //Validacion del usuario y la clave
+    if (user == "Naty" && clave == "12"){
+
+        //Con esto abriremos una nueva pagina de HTML
+        window.location=("pagina.html")
+    }
+}
+
+
+
+
+
+
+
+
+
+
